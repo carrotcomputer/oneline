92fe90325ee67efcf427a5d8e8ef1f595845e61f
Alan Adesalu <alan@sparkrevolutions.com>
2015-01-10 14:25:36 +0000

    Changes background colour on transition between conversations and conversation views

--- app/controllers/conversations.js
+++ app/controllers/conversations.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  needs: ['application'],
   actions: {
     goToConversation: function(){
       this.transitionToRoute('conversation');
--- app/routes/conversation.js
+++ app/routes/conversation.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  renderTemplate: function(controller){
+    this.render();
+    
+    controller.set('controllers.application.background', 'emerald-green-background');
+  }
 });
